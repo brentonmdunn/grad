f5028ba4828cd1a4444ed60adf941bce38f7485b
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2024-05-02 13:53:08 -0700

    Initial commit

---
+++ src/photos.js
@@ -0,0 +1,51 @@
+import img1 from './images/2023SeniorReEdit2-7909.jpg'
+import img2 from './images/2023SeniorReEdit2-8016.jpg'
+import img3 from './images/2023SeniorReEdit2-8317.jpg'
+import img4 from './images/2023SeniorReEdit2-8568.jpg'
+import img5 from './images/2023SeniorReEdit2-8695.jpg'
+import img6 from './images/2023SeniorReEdit2-8848.jpg'
+import img7 from './images/2023SeniorReEdit2-8998.jpg'
+import img8 from './images/2023SeniorReEdit2-9198.jpg'
+
+export const photos = [
+    {
+      src: img1,
+      width: 3,
+      height: 2
+    },
+    {
+      src: img2,
+      width: 2,
+      height: 3
+    },
+    {
+      src: img3,
+      width: 2,
+      height: 3
+    },
+    {
+      src: img4,
+      width: 3,
+      height: 2
+    },
+    {
+      src: img5,
+      width: 3,
+      height: 2
+    },
+    {
+      src: img6,
+      width: 2,
+      height: 3
+    },
+    {
+      src: img7,
+      width: 3,
+      height: 2
+    },
+    {
+      src: img8,
+      width: 2,
+      height: 3
+    }
+  ];
